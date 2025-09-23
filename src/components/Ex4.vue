37bac3a31324e82790d299b2d78ac6e646a3ae9c
nicca1225 <lo.nicholas1609@gmail.com>
2025-09-23 15:04:09 +0800

    ex3,ex4

--- src/components/Ex4.vue
+++ src/components/Ex4.vue
@@ -3,10 +3,18 @@
         data() {
             return {
                 // Add code here
+                id: "demo1", 
+                blueBox: true, 
+                blueBtn: true,  
             }
         },
         methods: { 
    			// Add code here
+            changecolour(){
+                this.blueBox = !this.blueBox; 
+                this.blueBtn = !this.blueBtn; 
+                
+            }
 		}
     }
 </script>
@@ -14,12 +22,13 @@
 <template>
     
     <!-- Modify code here -->
-    <div class="container">
-        <div v-bind:id="id" class="m-2">
+    <div v-bind:class="container">
+        <div v-bind:id="id" class="m-2" :class="{'blueBox': blueBox, 'redBox': !blueBox}">
             div ID : {{id}} 
         </div>
         
-        <button type="button">Change Color</button>
+        <button  :class="{'btn btn-primary': blueBtn, 'btn btn-danger': !blueBtn}" type="button"
+        @click="changecolour()">Change Color</button>
     </div>
 
 </template>
